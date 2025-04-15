097bea5f40da1b0cf97bfb1163a845b5802b5eb7
Andriy Halelyuka <43058552+andreydef@users.noreply.github.com>
2025-04-15 23:05:33 +0300

    move widget to iframe view + style fixes

--- index.js
+++ index.js
@@ -1,31 +1,5 @@
 import React from "react";
-import Frame from "react-frame-component";
 import { OCC2Widget } from "occ2-widget";
-
-function App() {
-  return (
-    <Frame
-      style={{ width: "100%", height: "100%", border: "none" }}
-      head={
-        <>
-          <link
-            href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
-            rel="stylesheet"
-          />
-          <link
-            rel="stylesheet"
-            href="https://unpkg.com/occ2-widget@latest/dist/index.css"
-          />
-        </>
-      }
-    >
-      <OCC2Widget theme="dark" position="right" isOpen={false} />
-    </Frame>
-  );
-}
-
-// Export the App component so it can be used by webpack
-export default App;
 
 // Wait for DOM to be fully loaded before attempting to render
 document.addEventListener("DOMContentLoaded", () => {
@@ -34,7 +8,7 @@ document.addEventListener("DOMContentLoaded", () => {
   if (rootElement) {
     import("react-dom/client").then(({ createRoot }) => {
       const root = createRoot(rootElement);
-      root.render(<App />);
+      root.render(<OCC2Widget theme="dark" position="right" isOpen={false} />);
     });
   } else {
     console.error(
--- index.php
+++ index.php
@@ -3,9 +3,6 @@
 
 <main class="main" id="main" role="main">
   <div class="container">
-    <!-- Add the widget container div here -->
-    <div id="occ2-widget-root"></div>
-
     <div class="content">
     		<?php get_template_part( 'loop', 'index' );	?>
     </div> 
